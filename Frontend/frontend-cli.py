34927ba8ac5ae77eae622fa152cf78f279214048
Ville <koivisto.vt@gmail.com>
2021-11-09 16:19:27 +0200

    Frontendiin komentoriviargumenttien validointi, eka versio

--- Frontend/frontend-cli.py
+++ Frontend/frontend-cli.py
@@ -4,7 +4,6 @@ Frontend vastaanottaa käyttäjältä tuntikirjauksia ja tallentaa ne kantaan (v
 Tuntikirjaukset yms. annetaan parametreinä pääfunktiolle
 """
 
-#TODO: syötteen validointi
 #TODO: tietokantaan yhteys
 #TODO: syöte tietokantaan
 
@@ -14,25 +13,111 @@ import psycopg2
 from datetime import datetime
 
 
-def validoi_data():
-    pass
+def validoi_data(args):
+    """ Käy läpi syötteen arvot ja validoi, että formaatti on oikea
+        - funktio purkaa argumentit listaan jossa indeksi
+          0: käyttäjän nimi (str),
+          1: aloituspvm (pp/kk/vvvv),
+          2: aloitusaika,
+          3: lopetuspvm (pp/kk/vvvv),
+          4: lopetusaika,
+          5: projektin nimi (str),
+          6: selite (mitä tulikaan tehtyä, str)
+    
+    """
+    kirjaukset = []
+
+    for arg in args:
+        kirjaukset.append(arg)
+
+    # Tarkistetaan onko nimi merkkijono
+    if kirjaukset[0].isdigit():
+        print(f"Käyttäjänimi ei voi olla numero: {kirjaukset[0]}")
+        
+        return False
+
+    # tarkistetaan päivämäärien formaatti
+    if not tarkista_pvm(kirjaukset[1], kirjaukset[3]):
+        print(f"Aloitus- tai lopetuspäivämäärä on väärässä formaatissa: {kirjaukset[1]}, {kirjaukset[3]}")
+        print("Oikea formaatti on: pp/kk/vvvv")
+
+        return False
+
+    # tarkistetaan aikojen formaatti
+    if not tarkista_aika(kirjaukset[2], kirjaukset[4]):
+        print(f"Aloitus- tai lopetusaika on väärässä formaatissa: {kirjaukset[2]}, {kirjaukset[4]}")
+        print("Oikea formaatti on: hh:mm")
+
+        return False
+
+    # Tarkistetaan onko projektin nimi merkkijono
+    if kirjaukset[5].isdigit():
+        print(f"Projektin nimi ei voi olla numero: {kirjaukset[5]}")
+        
+        return False
+
+    # Tarkistetaan onko selite merkkijono
+    if kirjaukset[6].isdigit():
+        print(f"Selite ei voi olla numero: {kirjaukset[6]}")
+        
+        return False
+
+    return True
 
 
-def main(kayttaja_id, aloitus_pvm, aloitus_aika, lopetus_pvm, lopetus_aika, projekti_nimi, selite):
+def tarkista_pvm(*args):
+    """ Tarkistaa, onko pvm oikeassa muodossa 
+        (validi datetime-objekti)
+    """
+    
+    for arg in args:
+        paiva, kuukausi, vuosi = arg.split("/")
+
+        try:
+            datetime(int(vuosi), int(kuukausi), int(paiva))
+
+        except ValueError:
+            return False
+        
+    return True
+
+
+def tarkista_aika(*args):
+    """ Tarkistaa, onko aika oikeassa muodossa 
+        (validi datetime-objekti)
+    """
+    
+    for arg in args:
+        try:
+            tunti, minuutti = arg.split(":")
+            tunti = int(tunti)
+            minuutti = int(minuutti)
+
+            if tunti < 0 or tunti > 23:
+                return False
+
+            elif minuutti < 0 or minuutti > 59:
+                return False
+
+        except ValueError:
+            return False
+
+    return True
+
+
+def main(*args):
     """ Pääfunktio josta käsin suoritetaan muut jutut """
 
-    # TESTI:
-    print(kayttaja_id)
-    print(aloitus_pvm)
+    # Ohjataan komentoriviargumentit validointifunktioon
+    validoi_data(args)
 
 
 if __name__ == "__main__":
-
     # Alustetaan parseri
     parser = argparse.ArgumentParser()
 
     # Lisätään argumentit
-    parser.add_argument("kayttaja_id")
+    parser.add_argument("kayttaja_nimi")
     parser.add_argument("aloitus_pvm")
     parser.add_argument("aloitus_aika")
     parser.add_argument("lopetus_pvm")
@@ -44,4 +129,8 @@ if __name__ == "__main__":
     argu = parser.parse_args()
 
     # Viedään argumentit mainiin
-    main(argu.kayttaja_id, argu.aloitus_pvm, argu.aloitus_aika, argu.lopetus_pvm, argu.lopetus_aika, argu.projekti_nimi, argu.selite)
+    main(argu.kayttaja_nimi, argu.aloitus_pvm, argu.aloitus_aika, argu.lopetus_pvm, argu.lopetus_aika, argu.projekti_nimi, argu.selite)
+
+
+# TESTAUKSEEN:
+# esimerkkikomento: python .\frontend-cli.py Ville 1/11/2021 8:30 2/11/2021 17:45 Awa-projekti "tein mitä tein"
