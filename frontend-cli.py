7199c091d99a4a0038a805cb3a9f6aafea50000a
Tiina <koodiit@gmail.com>
2021-11-09 11:35:00 +0200

    frontend runko täydennetty

--- frontend-cli.py
+++ frontend-cli.py
@@ -1,2 +1,16 @@
-def tuntikirjaus():
+"""
+Projektitehtävä / ryhmä 3 / Frontendin päätiedosto
+Frontend vastaanottaa käyttäjältä tuntikirjauksia ja tallentaa ne kantaan (vapaavalintainen tiedontallennusformaatti GCP:ssä)
+Tuntikirjaukset yms. annetaan parametreinä pääfunktiolle
+"""
+
+#TODO: syötteen validointi
+#TODO: tietokantaan yhteys
+#TODO: syöte tietokantaan
+
+def main():
+    """Pääfunktio josta käsin suoritetaan muut jutut"""
     pass
+
+if __name__ == "__main__":
+    main()
