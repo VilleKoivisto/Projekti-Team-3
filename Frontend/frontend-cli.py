2ee40d36cf0d68366372b90c40720c79ef12ccfd
Ville <koivisto.vt@gmail.com>
2021-11-09 14:39:53 +0200

    add: Frontendiin komentoriviargumentit

--- Frontend/frontend-cli.py
+++ Frontend/frontend-cli.py
@@ -8,9 +8,40 @@ Tuntikirjaukset yms. annetaan parametreinä pääfunktiolle
 #TODO: tietokantaan yhteys
 #TODO: syöte tietokantaan
 
-def main():
-    """Pääfunktio josta käsin suoritetaan muut jutut"""
+
+import argparse
+import psycopg2
+from datetime import datetime
+
+
+def validoi_data():
     pass
 
+
+def main(kayttaja_id, aloitus_pvm, aloitus_aika, lopetus_pvm, lopetus_aika, projekti_nimi, selite):
+    """ Pääfunktio josta käsin suoritetaan muut jutut """
+
+    # TESTI:
+    print(kayttaja_id)
+    print(aloitus_pvm)
+
+
 if __name__ == "__main__":
-    main()
+
+    # Alustetaan parseri
+    parser = argparse.ArgumentParser()
+
+    # Lisätään argumentit
+    parser.add_argument("kayttaja_id")
+    parser.add_argument("aloitus_pvm")
+    parser.add_argument("aloitus_aika")
+    parser.add_argument("lopetus_pvm")
+    parser.add_argument("lopetus_aika")
+    parser.add_argument("projekti_nimi")
+    parser.add_argument("selite")
+
+    # Käsitellään argumentit
+    argu = parser.parse_args()
+
+    # Viedään argumentit mainiin
+    main(argu.kayttaja_id, argu.aloitus_pvm, argu.aloitus_aika, argu.lopetus_pvm, argu.lopetus_aika, argu.projekti_nimi, argu.selite)
