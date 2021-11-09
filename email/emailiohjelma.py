e67a05a70dc01525338ce639a0155f21fb2c7f95
jajuhir <jhirvioja@gmail.com>
2021-11-09 14:35:23 +0200

    Lisätty emailohjelmaan säätiedosta minimum viable product

--- email/emailiohjelma.py
+++ email/emailiohjelma.py
@@ -1,6 +1,11 @@
 ## kirjastot
 
+# sähköposti
 import smtplib
+# sää
+import requests
+import json
+# muut
 from datetime import date
 
 ## configuroi emailin lähetyksen
@@ -26,6 +31,8 @@ with open(".emailsalaisuuksia","r") as f:
 # print(passu) # testiprinttejä
 
 ## kirjausdata mock, tähän tulee myöhemmin haku databasesta siltä päivältä milloin CRON-jobi suoritetaan
+## tähän myöhemmin looppi, että tunkee kaikki databasen tietyn päivän tiedot listaan tai sanakirjaan,
+## että ne voi kirjoittaa spostiviestiin
 
 aloituspvm = "9.11.2021"
 aloitusaika = "08:00"
@@ -34,20 +41,35 @@ lopetusaika = "16:00"
 projektinimi = "Projekti"
 selite = "Sikasaikku"
 tuntisumma = 25
-headerdate = date.today()
+subjectdate = date.today()
 
-## varsinainen sähköpostin lähetys
+## säätietojen haku
+
+api_key = "482cecd5122406c3e1ab27a830830dc3"
+zippikoodi = "00100" ## Helsinki / Kaisaniemi (todnäk)
+countrykoodi = "fi"
+langikoodi = "fi"
+url = "https://api.openweathermap.org/data/2.5/weather?zip=%s,%s&appid=%s&units=metric" % (zippikoodi, countrykoodi, api_key)
+response = requests.get(url)
+data = json.loads(response.text)
+lampotila_max = data['main']['temp_max']
+
+## varsinainen sähköpostin lähetys, tähän myöhemmin looppi että iteroi spostiviestiin kaikki kirjaukset listasta
 
 server.login(emaili, passu)
 sender_email = emaili
 receiver_email = "voicehaustmi@gmail.com"
-message = f"Subject: Emailraportti, {headerdate}" \
+message = f"Subject: Emailraportti, {subjectdate}" \
             f"\n" + "\n" \
             f"Aloituspvm- ja aika: {aloituspvm}, klo {aloitusaika} \n" \
             f"Lopetuspvm- ja aika: {lopetuspvm}, klo {lopetusaika} \n" \
             f"Projektinimi: {projektinimi} \n" \
             f"Selite: {selite}" \
             f"\n" + "\n" \
-            f"Tuntisumma: {tuntisumma}"
+            f"Tuntisumma: {tuntisumma}" \
+            f"\n" \
+            f"------------------------" \
+            f"\n" \
+            f"Hki (Kaisaniemen havaintoasema) celsiukset on {lampotila_max} C"
 
 server.sendmail(sender_email, receiver_email, message)
