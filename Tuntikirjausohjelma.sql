5f3b9d783e2db48c3d0628d40769033f4ab2d859
Ville <koivisto.vt@gmail.com>
2021-11-10 11:25:01 +0200

    vaihdettu nimet alkamaan pienillä kirjaimilla -> ei tuota konflikteja myöhemmin

--- Tuntikirjausohjelma.sql
+++ Tuntikirjausohjelma.sql
@@ -1,16 +1,16 @@
 
-CREATE DATABASE Tuntikirjausohjelma;
+CREATE DATABASE tuntikirjausohjelma;
 
 \c tuntikirjausohjelma;
 
 
-CREATE TABLE Tuntikirjaus (Id SERIAL PRIMARY KEY, 
-    Nimi VARCHAR(255) NOT NULL,
-    Aloituspvm date NOT NULL,
-    Aloitusaika time NOT NULL, 
-    Lopetuspvm date NOT NULL,
-    Lopetusaika time NOT NULL,
-    Projekti VARCHAR(255) NOT NULL,
-    Selite VARCHAR(255)
+CREATE TABLE tuntikirjaus (Id SERIAL PRIMARY KEY, 
+    nimi VARCHAR(255) NOT NULL,
+    aloituspvm date NOT NULL,
+    aloitusaika time NOT NULL, 
+    lopetuspvm date NOT NULL,
+    lopetusaika time NOT NULL,
+    projekti VARCHAR(255) NOT NULL,
+    selite VARCHAR(255)
 );
 
