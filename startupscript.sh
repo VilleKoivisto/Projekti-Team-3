2ad877a181e0d42f5dc6d6c5fe85de897b38c66d
miiaht <mhtakaw2021@gmail.com>
2021-11-10 16:29:23 +0200

    lisätty cronjob startupscriptiin

---
+++ startupscript.sh
@@ -0,0 +1,14 @@
+
+
+#! /bin/bash/
+
+sudo apt-get update
+# sudo apt-get install python 3.6 # ei tarvitse koska instanssissa on jo valmiiksi asennettuna python
+sudo apt-get -y install python-pip
+sudo apt-get -y install postgresql-client
+sudo pip install psycopg2-binary
+sudo gsutil cp gs://projektikoodit/[zippitiedostonnimi].zip .
+sudo unzip [zippitiedostonnimi].zip
+sudo crontab -e
+0 0 * * *  emailiohjelma.py
+
