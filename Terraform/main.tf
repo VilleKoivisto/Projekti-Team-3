21a7cf8a69289ce0cc1331611e2f9cf82e0b908e
jajuhir <jhirvioja@gmail.com>
2021-11-10 14:35:57 +0200

    Cloud SQL:llä nyt oma priva-IP, VPC-verkon cidr-range muutettu

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -35,7 +35,7 @@ resource "google_compute_network" "vpc_network" {
 # Luo tuntikirjaus-vpc:lle aliverkon tuntikirjaus-sub-fin, jossa 2 (käytettävissä olevaa) ip-osoitetta
 resource "google_compute_subnetwork" "public-subnetwork" {
     name = "tuntikirjaus-sub-fin"
-    ip_cidr_range = "10.0.0.0/29"
+    ip_cidr_range = "10.0.0.0/27"
     region = var.region
     network = google_compute_network.vpc_network.name
 }
@@ -54,15 +54,37 @@ resource "google_compute_firewall" "allow_ssh" {
   target_tags = ["ssh-enabled"]
 }
 
+# allokoi private ip osoitteet vpc:hen 
+resource "google_compute_global_address" "private_ip_do" {
+  name         = "private-ip"
+  purpose      = "VPC_PEERING"
+  address_type = "INTERNAL"
+  ip_version   = "IPV4"
+  prefix_length = 24
+  network       = google_compute_network.vpc_network.self_link
+}
+
+# enabloi private services access
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc_network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_do.name]
+}
+
 # Luo SQL database instancen annetuilla tiedoilla
 
 resource "google_sql_database_instance" "PostgresqlYenCHQMtfr" { # Jos cloud sql instanssi on tehty ja deletoitu/destroyattu, ei voi käyttää samaa instanssin nimeä viikkoon!
   name             = "postgresqlinstanssi"
   database_version = "POSTGRES_13"
+  depends_on       = [google_service_networking_connection.private_vpc_connection]
   region           = var.region
 
   settings {
     tier            = "db-f1-micro" # postgresql tukee vain shared core machineja! tämä shared-core löytyy haminasta
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc_network.self_link
+    }
   }
 }
 
