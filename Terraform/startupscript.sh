f91ab09acc842025cf87c9485246256509229da3
Tiina <koodiit@gmail.com>
2021-11-11 10:41:24 +0200

    Lisätty zip-tiedostojen haku ja purku ja muokattu cronjob

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -1,11 +1,17 @@
 #! /bin/bash/
 
 sudo apt-get update
-# sudo apt-get install python 3.6 # ei tarvitse koska instanssissa on jo valmiiksi asennettuna python
 sudo apt-get -y install python-pip
 sudo apt-get -y install postgresql-client
 sudo pip install psycopg2-binary
-sudo gsutil cp gs://projektikoodit/[zippitiedostonnimi].zip .
-sudo unzip [zippitiedostonnimi].zip
-sudo crontab -e
-0 0 * * *  emailiohjelma.py
+
+#haetaan bucketista zip-tiedostot ja unzipataan ne
+sudo gsutil cp gs://projektikoodit/Backend.zip .
+sudo gsutil cp gs://projektikoodit/Frontend.zip .
+sudo unzip Backend.zip
+sudo unzip Frontend.zip
+
+#tehdään tiedosto cronhomma, luodaan sinne cronjob (suorittaa emailiohjelma.py:n joka päivä 00:00) ja asennetaan uusi job
+sudo crontab -l > cronhomma
+sudo echo "0 0 * * *  emailiohjelma.py" >> cronhomma
+sudo crontab cronhomma
