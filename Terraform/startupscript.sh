1296164fe34447867f31942814c02ee3af23705a
Tiina <koodiit@gmail.com>
2021-11-11 11:15:55 +0200

    Lisätty unzip

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get -y install python-pip
 sudo apt-get -y install postgresql-client
 sudo pip install psycopg2-binary
+sudo apt-get install unzip
 
 #haetaan bucketista zip-tiedostot ja unzipataan ne
 sudo gsutil cp gs://projektikoodit/Backend.zip .
