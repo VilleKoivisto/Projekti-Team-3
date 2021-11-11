c8cf115878d158b190f82b9db3964fc4275128e6
Jaakko Hirvioja <83268566+jajuhir@users.noreply.github.com>
2021-11-11 15:50:03 +0200

    Muokatto "root" pois crontabin commandista

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get update && sudo apt-get -y install --upgrade python-pip && sudo apt-get -y install postgresql-client && sudo apt-get install unzip && sudo apt-get -y install --upgrade python3-pip && pip3 install --upgrade pip
 
-# lokaalit tarvittavat kirjastot eli meidän versio "requirements.txt"-tiedostosta
+# lokaalit tarvittavat kirjastot eli meidän versio "requirements.txt"-tiedostosta, tähän ei at-merkkeja koska voi tulla virheilmoja
 
 pip3 install psycopg2
 pip3 install requests
@@ -18,4 +18,4 @@ sudo unzip Frontend.zip -d ./frontendfolder
 
 #lisää cronjobin ilman että avaa editoria tms. muutenkaan tarvitsee hyväksyntää terminaalista
 
-sudo echo "0 0 * * * root python3 backendfolder/emailiohjelma.py" | crontab
+sudo echo "0 0 * * * python3 backendfolder/emailiohjelma.py" | crontab
