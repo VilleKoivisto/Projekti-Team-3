9dbeb10671606d7760665b5f5ee74b01f4804b5d
Tiina <koodiit@gmail.com>
2021-11-11 12:34:28 +0200

    pip päivitys, unzippaus kansioon

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -1,7 +1,7 @@
 #! /bin/bash/
 
 sudo apt-get update
-sudo apt-get -y install python-pip
+sudo apt-get -y install --upgrade python3-pip
 sudo apt-get -y install postgresql-client
 sudo pip install psycopg2-binary
 sudo apt-get install unzip
@@ -9,8 +9,8 @@ sudo apt-get install unzip
 #haetaan bucketista zip-tiedostot, unzipataan ne
 sudo gsutil cp gs://projektikoodit/Backend.zip .
 sudo gsutil cp gs://projektikoodit/Frontend.zip .
-sudo unzip Backend.zip
-sudo unzip Frontend.zip
+sudo unzip Backend.zip -d ./Backend
+sudo unzip Frontend.zip -d ./Frontend
 
 #tehdään tiedosto cronhomma, luodaan sinne cronjob (suorittaa emailiohjelma.py:n joka päivä 00:00) ja asennetaan uusi job
 sudo crontab -l > cronhomma
