4290f55bf4e99774329466fefcf9b77de8c600d1
Jaakko Hirvioja <83268566+jajuhir@users.noreply.github.com>
2021-11-11 15:39:58 +0200

    Startupscript valmisteltu testiympäristöä varten

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -1,20 +1,21 @@
 #! /bin/bash/
 
-sudo apt-get update
-sudo apt-get -y install --upgrade python3-pip
-sudo apt-get -y install postgresql-client
-sudo pip3 install psycopg2-binary
-sudo pip3 install requests
-sudo apt-get install unzip
+# root userille menevät tarvittavat jutut
 
+sudo apt-get update && sudo apt-get -y install --upgrade python-pip && sudo apt-get -y install postgresql-client && sudo apt-get install unzip && sudo apt-get -y install --upgrade python3-pip && pip3 install --upgrade pip
+
+# lokaalit tarvittavat kirjastot eli meidän versio "requirements.txt"-tiedostosta
+
+pip3 install psycopg2
+pip3 install requests
 
 #haetaan bucketista zip-tiedostot, unzipataan ne
+
 sudo gsutil cp gs://projektikoodit/Backend.zip .
 sudo gsutil cp gs://projektikoodit/Frontend.zip .
 sudo unzip Backend.zip -d ./backendfolder
 sudo unzip Frontend.zip -d ./frontendfolder
 
-#tehdään tiedosto cronhomma, luodaan sinne cronjob (suorittaa emailiohjelma.py:n joka päivä 00:00) ja asennetaan uusi job
-sudo crontab -l > cronhomma
-sudo echo "0 0 * * *  emailiohjelma.py" >> cronhomma
-sudo crontab cronhomma
+#lisää cronjobin ilman että avaa editoria tms. muutenkaan tarvitsee hyväksyntää terminaalista
+
+sudo echo "0 0 * * * root python3 backendfolder/emailiohjelma.py" | crontab
