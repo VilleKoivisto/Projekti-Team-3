e1efd50bfe2150bfd0898307a32aad8f4669b643
Tiina <koodiit@gmail.com>
2021-11-11 12:50:56 +0200

    Requests-kirjaston asennus

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -3,14 +3,16 @@
 sudo apt-get update
 sudo apt-get -y install --upgrade python3-pip
 sudo apt-get -y install postgresql-client
-sudo pip install psycopg2-binary
+sudo pip3 install psycopg2-binary
+sudo pip3 install requests
 sudo apt-get install unzip
+
 
 #haetaan bucketista zip-tiedostot, unzipataan ne
 sudo gsutil cp gs://projektikoodit/Backend.zip .
 sudo gsutil cp gs://projektikoodit/Frontend.zip .
-sudo unzip Backend.zip -d ./Backend
-sudo unzip Frontend.zip -d ./Frontend
+sudo unzip Backend.zip -d ./backendfolder
+sudo unzip Frontend.zip -d ./frontendfolder
 
 #tehdään tiedosto cronhomma, luodaan sinne cronjob (suorittaa emailiohjelma.py:n joka päivä 00:00) ja asennetaan uusi job
 sudo crontab -l > cronhomma
