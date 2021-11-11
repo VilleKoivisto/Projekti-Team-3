dcb0e6cd27dbfbcfec07616b92c5dbb29d1eac3a
miiaht <mhtakaw2021@gmail.com>
2021-11-11 08:24:27 +0200

    päivitetty startupscript

--- Terraform/startupscript.sh
+++ Terraform/startupscript.sh
@@ -7,3 +7,5 @@ sudo apt-get -y install postgresql-client
 sudo pip install psycopg2-binary
 sudo gsutil cp gs://projektikoodit/[zippitiedostonnimi].zip .
 sudo unzip [zippitiedostonnimi].zip
+sudo crontab -e
+0 0 * * *  emailiohjelma.py
